f4294820e5ee0d711059762fb365aa25744e18e3
Abglaa <Altymysbaevabdulaziz@gmail.com>
2023-11-21 20:56:04 +0600

    homework4

--- src/App.js
+++ src/App.js
@@ -1,107 +1,85 @@
-import './App.css';
-import {useEffect, useState} from "react";
+import React, { useState } from "react";
+import "./App.css";
 import movie from './movie/video.mp4'
-const App = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
 
-    const [nameDirty, setNameDirty] = useState(false)
-    const [emailDirty, setEmailDirty] = useState(false)
-    const [passwordDirty, setPasswordDirty] = useState(false)
+export default function App() {
+  const [version, setVersion] = useState(0);
 
-    const [nameError, setNameError] = useState('Вы забыли написать имя')
-    const [emailError, setEmailError] = useState('Вы забыли написать gmail')
-    const [passwordError, setPasswordError] = useState('Вы забыли написать пароль')
-    const [formValid, setFormValid] = useState(false)
+  function handleReset() {
+    setVersion(version + 1);
+  }
 
-    useEffect(() => {
-        if (emailError || passwordError) {
-            setFormValid(false)
-        } else {
-            setFormValid(true)
-        }        
-    }, [emailError, passwordError]);
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log("Submitted data:", { name, email, password });
-      };
-
-    const logFormData = () => {
-        console.log(name, email, password);
-      };
-
-    const emailHandler = (e) =>{
-        setEmail(e.target.value)
-        const regExp = /^[a-zA-Z0-9._%+-]+@email.com$/;
-        if (!regExp.test(String(e.target.value).toLowerCase())){
-            setEmailError('Некорректный email')
-        } else {
-            setEmailError("")
-        }
-    }
-    const nameHandler = (e) =>{
-        setName(e.target.value)
-        const regExp = /^[a-zA-Zа-яА-Я0-9._%+-]/;
-        if (!regExp.test(String(e.target.value).toLowerCase())){
-            setNameError('Некорректный имя')
-        } else {
-            setNameError("")
-        }
-    }
-    const passwordHandler = (e) => {
-        setPassword(e.target.value)
-        if (e.target.value.length < 3 || e.target.value.length > 12) {
-            setPasswordError('Пароль должен быть длиннее 3 и меньше 12')
-            if (!e.target.value) {
-                setPasswordError('Вы забыли написать пароль')
-            }
-        } else {
-            setPasswordError("")
-        }
-    }
-
-
-    const blurHandler = (e) => {
-        switch (e.target.name) {
-            case 'name':
-                setNameDirty(true)
-                break
-            case 'email':
-                setEmailDirty(true)
-                break
-            case 'password':
-                setPasswordDirty(true)
-                break
-                default:
-                    break
-        }
-    }
-
-    return (
-        <div className="app">
-            <video autoPlay loop muted className="bg-video">
-                <source src={movie} type="video/mp4"/>
-            </video>
-            <form>
-                <div className="sign_form">
-                    <h1>Авторизация</h1>
-                    {(nameDirty && nameError) && <div style={{color: 'red'}}>{nameError}</div>}
-                    <input onChange={e => nameHandler(e)} value={name} onBlur={e => blurHandler(e)} name='name' type="text" placeholder='Enter your name....'/>
-
-                    {(emailDirty && emailError) && <div style={{color: 'red'}}>{emailError}</div>}
-                    <input onChange={e => emailHandler(e)} value={email} onBlur={e => blurHandler(e)} name='email' type="text" placeholder='Enter your email....'/>
-
-                    {(passwordDirty && passwordError) && <div style={{color: 'red'}}>{passwordError}</div>}
-                    <input onChange={e => passwordHandler(e)} value={password} onBlur={e => blurHandler(e)} name='password' type="password" placeholder='Enter your password....'/>
-
-                    <button onClick={handleSubmit} disabled={!formValid} type={"submit"}>submit</button>
-                </div>
-            </form>
-
-        </div>
-    )
+  return (
+    <>
+    <div className="center">
+        <video autoPlay loop muted className="bg-video">
+            <source src={movie} type="video/mp4"/>
+        </video>
+      <button className="reset" onClick={handleReset}><h1>Авторизация</h1></button>
+      <Form key={version} />
+    </div>
+    </>
+  );
 }
-export default App;
 
+function Form() {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    phoneNumber: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Представленные данные:", formData);
+  };
+
+  return (
+    <form className="registration-form" onSubmit={handleSubmit}>
+      <label>
+        Full Name:
+        <input
+          type="text"
+          name="fullName"
+          value={formData.fullName}
+          onChange={handleChange}
+          placeholder="Enter your name....."
+        />
+      </label>
+      <label>
+        Phone Number:
+        <input
+          type="number"
+          name="phoneNumber"
+          value={formData.phoneNumber}
+          onChange={handleChange}
+          placeholder="Enter your number...."
+          
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="Enter you @email"
+        />
+      </label>
+      <button className="btn" type="submit">Submit</button>
+
+      <div>
+        <p>Name: {formData.fullName}</p>
+        <p>Phone: {formData.phoneNumber}</p>
+        <p>Email: {formData.email}</p>
+      </div>
+    </form>
+  );
+}
